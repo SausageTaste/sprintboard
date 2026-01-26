21ad918e85929fc451b3fce81a18e24b742bddb4
Sungmin Woo <woos8899@gmail.com>
2026-01-26 09:51:38 +0900

    Redirect root path to /images in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Gallery from "./Gallery";
 
 export default function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/images" replace />} />
       <Route path="/images/*" element={<Gallery />} />
       <Route path="*" element={<div style={{ padding: 16 }}>Not found. Try <a href="/images">/images</a></div>} />
     </Routes>
