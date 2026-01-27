d8bf2e629366576aee34b570357927b0c62759de
Sungmin Woo <woos8899@gmail.com>
2026-01-27 23:42:18 +0900

    `PowerRequest` class

--- src_cpp/server/src/util/wake.cpp
+++ src_cpp/server/src/util/wake.cpp
@@ -3,6 +3,46 @@
 #include <windows.h>
 
 
+// PowerRequest
+namespace sung {
+
+    PowerRequest::PowerRequest(const wchar_t* reason) {
+        REASON_CONTEXT rc{};
+        rc.Version = POWER_REQUEST_CONTEXT_VERSION;
+        rc.Flags = POWER_REQUEST_CONTEXT_SIMPLE_STRING;
+        rc.Reason.SimpleReasonString = const_cast<wchar_t*>(reason);
+
+        handle_ = PowerCreateRequest(&rc);
+    }
+
+    PowerRequest::~PowerRequest() {
+        if (this->ok())
+            CloseHandle(handle_);
+    }
+
+    bool PowerRequest::ok() const {
+        return handle_ != nullptr && handle_ != INVALID_HANDLE_VALUE;
+    }
+
+    bool PowerRequest::set_system_required(bool on) {
+        return this->ok() &&
+               PowerSetRequest(handle_, PowerRequestSystemRequired) == TRUE;
+    }
+
+    bool PowerRequest::set_display_required(bool on) {
+        return this->ok() &&
+               PowerSetRequest(handle_, PowerRequestDisplayRequired) == TRUE;
+    }
+
+    // Call clear when done
+    bool PowerRequest::clear_system_required() {
+        return this->ok() &&
+               PowerClearRequest(handle_, PowerRequestSystemRequired) == TRUE;
+    }
+
+}  // namespace sung
+
+
 namespace sung {
 
     double get_idle_time() {
--- src_cpp/server/src/util/wake.hpp
+++ src_cpp/server/src/util/wake.hpp
@@ -3,6 +3,23 @@
 
 namespace sung {
 
+    class PowerRequest {
+
+    public:
+        PowerRequest(const wchar_t* reason);
+        ~PowerRequest();
+
+        bool ok() const;
+
+        bool set_system_required(bool on);
+        bool set_display_required(bool on);
+        bool clear_system_required();
+
+    private:
+        void* handle_ = nullptr;
+    };
+
+
     double get_idle_time();
 
 }  // namespace sung
