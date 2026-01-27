62bd27fd783414cf85540ad1636248a35d80dc88
Sungmin Woo <woos8899@gmail.com>
2026-01-27 10:21:16 +0900

    Try getting AVIF prompt data in main server

--- src_cpp/server/src/main.cpp
+++ src_cpp/server/src/main.cpp
@@ -6,6 +6,7 @@
 #include "sung/auxiliary/comfyui_data.hpp"
 #include "sung/auxiliary/filesys.hpp"
 #include "sung/auxiliary/server_configs.hpp"
+#include "sung/image/avif.hpp"
 #include "sung/image/png.hpp"
 #include "util/simple_img_info.hpp"
 
@@ -195,19 +196,37 @@ namespace {
     };
 
 
-    std::string get_prompt(
+    std::string get_prompt_no_catch(
         const sung::SimpleImageInfo& info, const sung::Path& file_path
     ) {
         if (info.is_png()) {
-            try {
-                const auto meta = sung::read_png_metadata_only(file_path);
-                if (auto wf = meta.find_text_chunk("workflow")) {
-                    return sung::find_prompt(wf->data(), wf->size());
-                }
-            } catch (const std::exception& e) {
-                return "";
+            const auto meta = sung::read_png_metadata_only(file_path);
+            if (auto wf = meta.find_text_chunk("workflow")) {
+                return sung::find_prompt(wf->data(), wf->size());
             }
+        } else if (info.is_avif()) {
+            const auto file_content = sung::read_file(file_path);
+            if (file_content.empty())
+                return "";
+            const auto avif_meta = sung::read_avif_metadata_only(
+                file_content.data(), file_content.size()
+            );
+            const auto workflow = avif_meta.find_workflow_data();
+            if (workflow.empty())
+                return "";
+
+            return sung::find_prompt(workflow.data(), workflow.size());
         } else {
+            return "";
+        }
+    }
+
+    std::string get_prompt(
+        const sung::SimpleImageInfo& info, const sung::Path& file_path
+    ) {
+        try {
+            return get_prompt_no_catch(info, file_path);
+        } catch (const std::exception& e) {
             return "";
         }
     }
