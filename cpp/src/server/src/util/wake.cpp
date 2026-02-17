5f2c2e8ae7fcb41b0b47025677b4e49862884c1c
Sungmin Woo <woos8899@gmail.com>
2026-02-17 11:27:30 +0900

    Disable Windows only code

--- cpp/src/server/src/util/wake.cpp
+++ cpp/src/server/src/util/wake.cpp
@@ -2,31 +2,40 @@
 
 #include <print>
 
-#include <windows.h>
+#ifdef _WIN32
+    #include <windows.h>
+#endif
 
 
 // PowerRequest
 namespace sung {
 
     PowerRequest::PowerRequest(const wchar_t* reason) {
+#ifdef _WIN32
         REASON_CONTEXT rc{};
         rc.Version = POWER_REQUEST_CONTEXT_VERSION;
         rc.Flags = POWER_REQUEST_CONTEXT_SIMPLE_STRING;
         rc.Reason.SimpleReasonString = const_cast<wchar_t*>(reason);
 
         handle_ = PowerCreateRequest(&rc);
+#endif
     }
 
     PowerRequest::~PowerRequest() {
+#ifdef _WIN32
         if (this->ok())
             CloseHandle(handle_);
+#endif
     }
 
     bool PowerRequest::ok() const {
+#ifdef _WIN32
         return handle_ != nullptr && handle_ != INVALID_HANDLE_VALUE;
+#endif
     }
 
     bool PowerRequest::set_system_required(bool on) {
+#ifdef _WIN32
         if (!this->ok())
             return false;
         if (TRUE != PowerSetRequest(handle_, PowerRequestSystemRequired))
@@ -34,9 +43,12 @@ namespace sung {
 
         system_required_ = true;
         return true;
+#endif
+        return false;
     }
 
     bool PowerRequest::set_display_required(bool on) {
+#ifdef _WIN32
         if (!this->ok())
             return false;
         if (TRUE != PowerSetRequest(handle_, PowerRequestDisplayRequired))
@@ -44,9 +56,12 @@ namespace sung {
 
         display_required_ = true;
         return true;
+#endif
+        return false;
     }
 
     bool PowerRequest::clear_system_required() {
+#ifdef _WIN32
         if (!this->ok())
             return false;
         if (TRUE != PowerClearRequest(handle_, PowerRequestSystemRequired))
@@ -54,9 +69,13 @@ namespace sung {
 
         system_required_ = false;
         return true;
+#else
+        return false;
+#endif
     }
 
     bool PowerRequest::clear_display_required() {
+#ifdef _WIN32
         if (!this->ok())
             return false;
         if (TRUE != PowerClearRequest(handle_, PowerRequestDisplayRequired))
@@ -64,6 +83,9 @@ namespace sung {
 
         display_required_ = false;
         return true;
+#else
+        return false;
+#endif
     }
 
 }  // namespace sung
@@ -118,6 +140,7 @@ namespace sung {
 namespace sung {
 
     double get_idle_time() {
+#ifdef _WIN32
         LASTINPUTINFO lii{};
         lii.cbSize = sizeof(lii);
 
@@ -125,6 +148,9 @@ namespace sung {
 
         const auto ms = GetTickCount() - lii.dwTime;
         return ms / 1000.0;
+#else
+        return 0.0;
+#endif
     }
 
 }  // namespace sung
